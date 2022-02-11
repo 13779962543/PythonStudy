c08ca9d66e3c8ca1a297c7e90df463b4698a7eeb
13779962543 <1507543567@qq.com>
2022-02-11 13:20:36 +0800

    test.html

--- 爬虫/数据解析/xpath解析案例-解析下载图片爬取.py
+++ 爬虫/数据解析/xpath解析案例-解析下载图片爬取.py
@@ -5,34 +5,34 @@ import urllib.request
 
 if __name__=="__main__":
 
-    url_main='https://pic.netbian.com/4kmeinv/'
-    x = input('请输入页数,注意，爬取的页数要比输入的页数少一页，如要爬取五页，请输入 6 ：')
+    url='https://pic.netbian.com/4kmeinv/'
+
     # 循环输出页数
-    for num in range(1, int(x)):
-        url='https://pic.netbian.com/4kmeinv/index_'+str(num)+'.html'
-        print(url)
-        headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36 Edg/98.0.1108.43'
-        }
-        response=requests.get(url=url,headers=headers)
-        #手动设定响应数据的编码格式
-        response.encoding=response.apparent_encoding
-        data=response.text
-        #数据解析:src属性值，alt属性值
-        tree=etree.HTML(data)
-        li_list=tree.xpath('//div[@class="slist"]/ul/li')
-        if not os.path.exists('./beautifulgirl'):
-            os.mkdir('./beautifulgirl')
-        for li in li_list:
-            img_src='https://pic.netbian.com/'+li.xpath('./a/img/@src')[0]
-            img_name=li.xpath('./a/img/@alt')[0]+'.jpg'
-            #通用处理中文乱码的解决方法
-            # img_name=img_name.encode('iso-8859-1').decode('gbk')
-            #请求图片进行持久化处理
-            img_data=requests.get(url=img_src,headers=headers).content
-            img_path='beautifulgirl/'+img_name
-            with open(img_path,'wb')as fp:
-                fp.write(img_data)
-                print(img_name,'下载成功')
+
+
+    print(url)
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36 Edg/98.0.1108.43'
+    }
+    response=requests.get(url=url,headers=headers)
+    #手动设定响应数据的编码格式
+    response.encoding=response.apparent_encoding
+    data=response.text
+    #数据解析:src属性值，alt属性值
+    tree=etree.HTML(data)
+    li_list=tree.xpath('//div[@class="slist"]/ul/li')
+    if not os.path.exists('./beautifulgirl'):
+        os.mkdir('./beautifulgirl')
+    for li in li_list:
+        img_src='https://pic.netbian.com/'+li.xpath('./a/img/@src')[0]
+        img_name=li.xpath('./a/img/@alt')[0]+'.jpg'
+        #通用处理中文乱码的解决方法
+        # img_name=img_name.encode('iso-8859-1').decode('gbk')
+        #请求图片进行持久化处理
+        img_data=requests.get(url=img_src,headers=headers).content
+        img_path='beautifulgirl/'+img_name
+        with open(img_path,'wb')as fp:
+            fp.write(img_data)
+            print(img_name,'下载成功')
 
 
